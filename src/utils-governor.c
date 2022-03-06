c9ba58aa1fdcd8aeb61d8843f3977ca693410ba6
mike <mikefc@coolbutuseless.com>
2022-03-06 16:15:55 +1000

    tidy fps_governor/sleep

---
+++ src/utils-governor.c
@@ -0,0 +1,73 @@
+
+
+
+#include <R.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This is a "good enough" FPS governor in C
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SEXP fps_governor_(SEXP fps_) {
+  static int init = 0;
+  static double fps = 30;
+  static struct timeval last_time;
+  static struct timeval checkpoint_time;
+  struct timeval this_time;
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // On first call just init everything, but don't wait around.
+  // This is "good enough" for now. Mike 2022-03-01
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (init == 0) {
+    init++;
+    gettimeofday(&last_time      , NULL);
+    gettimeofday(&checkpoint_time, NULL);
+    fps = asReal(fps_);
+    return ScalarReal(fps);
+  }
+
+  init++;
+  gettimeofday(&this_time, NULL);
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Simple fps averaged over the last 10 frames
+  // This is "good enough" for now. Mike 2022-03-01
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (init % 10 == 0) {
+    double actual = (this_time.tv_sec + this_time.tv_usec/1000000.0) -
+      (checkpoint_time.tv_sec + checkpoint_time.tv_usec/1000000.0);
+    fps = 10 / actual;
+    memcpy(&checkpoint_time, &this_time, sizeof(struct timeval));
+    // Rprintf(">> %.2f", fps);
+  }
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Find the current time and work our how long to wait
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  double actual = (this_time.tv_sec + this_time.tv_usec/1000000.0) -
+    (last_time.tv_sec + last_time.tv_usec/1000000.0);
+  double expected = 1.0/asReal(fps_);
+  double wait = (expected - actual) * 1e6;
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Factor in a tiny bit of overhead and 'wait' for this amount of time.
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (wait > 3000) {
+    usleep(wait - 3000);
+  }
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Reset the clock so we can get an accurate frame time next run
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  gettimeofday(&last_time, NULL);
+
+  return ScalarReal(fps);
+}
